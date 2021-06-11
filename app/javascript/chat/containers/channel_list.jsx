3f0c0454aee37f4dc8f3d38b23312fac505ad670
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-11 12:19:20 +0400

    added action cable url for production

--- app/javascript/chat/containers/channel_list.jsx
+++ app/javascript/chat/containers/channel_list.jsx
@@ -27,7 +27,7 @@ class ChannelList extends Component {
   render() {
     return (
       <div className="channels-container">
-        <span>Not Slack Chat</span>
+        <span>Channels:</span>
         <ul>
           {this.props.channels.map(this.renderChannel)}
         </ul>
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://rails--chat-redux.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://rails--chat-redux.herokuapp.com', 'http://rails--chat-redux.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
